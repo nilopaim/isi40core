f694f283e2f191a9fc35f6111b690760d00c6539
Nilo Roberto C Paim <nilopaim@gmail.com>
2023-08-10 13:41:21 -0300

    created PROTOCOL struct

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package isi40core
 
 import "log"
 
+type Protocol struct {
+	ProtocolName string
+}
+
+func (p *Protocol) GetProtocolName() string {
+	return p.ProtocolName
+}
+
+func (p *Protocol) SetProtocolName(protocolName string) {
+	p.ProtocolName = protocolName
+}
+
+func (p *Protocol) Init() {
+	LogInfo("Initializing protocol " + p.ProtocolName)
+}
+
 func LogInfo(message string) {
 	log.Printf("INFO - %v", message)
 }
