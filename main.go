a87591e80e95c8a53d5faa0f5674e00476a6c5b2
Nilo Roberto C Paim <nilopaim@gmail.com>
2023-08-10 13:51:42 -0300

    added newprotocol func

--- main.go
+++ main.go
@@ -3,19 +3,27 @@ package isi40core
 import "log"
 
 type Protocol struct {
-	ProtocolName string
+	Name    string
+	Version string
+}
+
+func NewProtocol(protocolName string, protocolVersion string) *Protocol {
+	protocol := new(Protocol)
+	protocol.Name = protocolName
+	protocol.Version = protocolVersion
+	return protocol
 }
 
 func (p *Protocol) GetProtocolName() string {
-	return p.ProtocolName
+	return p.Name
 }
 
-func (p *Protocol) SetProtocolName(protocolName string) {
-	p.ProtocolName = protocolName
+func (p *Protocol) GetProtocolVersion() string {
+	return p.Version
 }
 
 func (p *Protocol) Init() {
-	LogInfo("Initializing protocol " + p.ProtocolName)
+	LogInfo("Initializing protocol " + p.Name)
 }
 
 func LogInfo(message string) {
